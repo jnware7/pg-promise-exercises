e8112993d2f7bfe21ae8151c72f9b9ec5b96d188
jnware7 <jnware7@gmail.com>
2017-03-03 21:50:03 -0800

    Pg-promise Exercises

--- exercises.js
+++ exercises.js
@@ -5,7 +5,7 @@ const postgresConfig = {
   host: 'localhost',
   port: 5432,
   database: 'pg-promise-exercises',
-  user: '<change-this-to-your-username>', // replace this with your username
+  user: 'Mr.Ware', // replace this with your username
   password: '' //  replace this if you have set a password for your username (this is unlikely)
 };
 
@@ -31,7 +31,7 @@ const allBooks = db.any('select * from books')
 /* This is calling the `then` function on the `allBooks` promise, and checks if
    we get back 15 rows. This assertion will fail. Make it PASS!*/
 allBooks.then(books => {
-  assert.deepEqual(books.length, 20)
+  assert.deepEqual(books.length, 15)
 }).catch(error => {
   console.log('Dang, my assertion failed.', error);
 });
@@ -41,7 +41,7 @@ allBooks.then(books => {
 
 
 
-
+//
 /* -----------------------------------------
            Exercise 2
    -----------------------------------------
@@ -55,7 +55,7 @@ allBooks.then(books => {
 
 */
 
-let firstTenBooks; // = .... IMPLEMENT THIS FUNCTION
+let firstTenBooks = db.any('SELECT * FROM books LIMIT 10 ')//  = .... IMPLEMENT THIS FUNCTION
 firstTenBooks.then(books => {
   assert(books.length, 10)
 }).catch(error => {
@@ -65,25 +65,25 @@ firstTenBooks.then(books => {
 /* --------End of Exercise 2---------------- */
 
 
+//
+//
+// /* -----------------------------------------
+//             Exercise 3
+//    -----------------------------------------
+// //
+//    Implement the function `findAuthorsOrderedByLastName` which returns all the
+//    authors from the the `authors` table, and the rows are ordered by the
+//    `last_name`.
+//
+//
+//    @function: `findAuthorsOrderedByLastName`
+//    @input params: None
+//    @output: [{id, first_name, last_name}]
+//
+//
+// */
 
-
-/* -----------------------------------------
-            Exercise 3
-   -----------------------------------------
-
-   Implement the function `findAuthorsOrderedByLastName` which returns all the
-   authors from the the `authors` table, and the rows are ordered by the
-   `last_name`.
-
-
-   @function: `findAuthorsOrderedByLastName`
-   @input params: None
-   @output: [{id, first_name, last_name}]
-
-
-*/
-
-let findAuthorsOrderedByLastName; // = .... IMPLEMENT THIS FUNCTION
+let findAuthorsOrderedByLastName = db.any('SELECT last_name FROM authors ORDER BY last_name ASC')
 findAuthorsOrderedByLastName.then(authors => {
   assert.deepEqual(authors.length, 19)
   assert.deepEqual(authors[0].last_name, 'Alcott')
@@ -91,134 +91,158 @@ findAuthorsOrderedByLastName.then(authors => {
 }).catch(error => {
   console.log('Whoops, my function doesnt behave as expected.', error);
 });
+//
+// /* --------End of Exercise 3---------------- */
+//
+//
+//
+// /* -----------------------------------------
+//    Exercise 4
+//    -----------------------------------------
+//
+//    Implement the function `findBookAuthors` which returns the `first_name` and
+//    `last_name` from the `authors` table, and the `title` of the
+//    books(from the `books` table) that the authors have written.
+//
+//    @function: `findBookAuthors`
+//    @input params: None
+//    @output: [{first_name, last_name, title}]
+//
+//    In this exercise you will ALSO have to write the assertions. For inspiration,
+//    look at the assertions in Exercises 1 - 3.
+//
+//    Expected Result:
+//    [{first_name: 'John', last_name: 'Worsley', title: 'Practical PostgreSQL'}
+//    {first_name: 'Paulette', last_name: 'Bourgeois', title: 'Franklin in the Dark'}
+//    {first_name: 'Margery Williams', last_name: 'Bianco', title: 'The Velveteen Rabbit'}
+//    {first_name: 'Louisa May', last_name: 'Alcott', title: 'Little Women'}
+//    {first_name: 'Stephen', last_name: 'King', title: 'The Shining'}
+//    {first_name: 'Frank', last_name: 'Herbert', title: 'Dune'}
+//    {first_name: 'Burne', last_name: 'Hogarth', title: 'Dynamic Anatomy'}
+//    {first_name: 'Margaret Wise', last_name: 'Brown', title: 'Goodnight Moon'}
+//    {first_name: 'Edgar Allen', last_name: 'Poe', title: 'The Tell-Tale Heart'}
+//    {first_name: 'Mark', last_name: 'Lutz', title: 'Learning Python'}
+//    {first_name: 'Mark', last_name: 'Lutz', title: 'Programming Python'}
+//    {first_name: 'Tom', last_name: 'Christiansen', title: 'Perl Cookbook'}
+//    {first_name: 'Arthur C.', last_name: 'Clarke', title: '2001: A Space Odyssey'}
+//    {first_name: 'Theodor Seuss', last_name: 'Geisel', title: 'Bartholomew and the Oobleck'}
+//    {first_name: 'Theodor Seuss', last_name: 'Geisel', title: 'The Cat in the Hat'}]
+// */
+let findBookAuthors = db.any('SELECT a.first_name, a.last_name,b.title FROM authors AS a INNER JOIN books AS b ON a.id = b.author_id')
+findBookAuthors.then((results)=>{
+  assert.deepEqual(results.length, 15)
+  assert.deepEqual(results[0].first_name,'John')
+  assert.deepEqual(results[results.length - 1].last_name,'Geisel')
+})
+console.log(findBookAuthors)
+//
+// /* --------End of Exercise 4---------------- */
+//
+//
+//
+//
+//
+// /* -----------------------------------------
+//    Exercise 5
+//    -----------------------------------------
+//
+//    Implement the function `authorIdWithTwoBooks` which returns the
+//    `author_id` of authors who have 2 books. (HINT: you have to use a SUBQUERY)
+//
+//    @function: `authorIdWithTwoBooks`
+//    @input params: None
+//    @output: [{first_name, last_name, title}]
+//
+//    In this exercise you will ALSO have to write the assertions. For inspiration,
+//    look at the assertions in Exercises 1 - 3.
+//
+//    Expected Result:
+//      [{author_id: 1809},
+//       {author_id: 7805}]
+//
+//
+// */
+let authorIdWithTwoBooks = db.any('SELECT * FROM (SELECT author_id FROM books GROUP BY author_id HAVING COUNT(*) > 1) sub')
 
-/* --------End of Exercise 3---------------- */
+authorIdWithTwoBooks.then( (results) => {
+    assert.deepEqual(results.length, 2)
+    assert.deepEqual(results[0].author_id ,1809)
+})
+//
+// /* --------End of Exercise 5---------------- */
+//
+//
+//
+//
+//
+// /* -----------------------------------------
+//    Exercise 6
+//    -----------------------------------------
+//
+//    Implement the function `bookTitlesWithMultipleEditions` which returns the
+//    `title` of books which have more than 2 editions. (HINT: you have to use a join)
+//
+//    @function: `bookTitlesWithMultipleEditions`
+//    @input params: None
+//    @output: [{title}]
+//
+//    In this exercise you will ALSO have to write the assertions. For inspiration,
+//    look at the assertions in Exercises 1 - 3.
+//
+//    Expected Result:
+//      [{title: 'The Shining'},
+//       {title: 'The Cat in the Hat'},
+//       {title: 'Dune'}
+//       {title: '2001: A Space Odyssey'}
+//       {title: 'The Tell-Tale Heart'}]
+//b is of books e is for editions
+// */
+let bookTitlesWithMultipleEditions = db.any('SELECT b.title FROM books b INNER JOIN editions e ON e.book_id = b.id GROUP BY b.id HAVING COUNT(*) > 1')
 
+bookTitlesWithMultipleEditions.then((results)=>{
+  console.log(results)
+})
+// /* --------End of Exercise 6---------------- */
+ //
+//
+//
+//
+// /* -----------------------------------------
+//    Exercise 7
+//    -----------------------------------------
+//
+//    Implement the function `findStockedBooks` which returns the `title` & the
+//    author's `first_name` & `last_name` of all books which are stocked as
+//    represented in the `daily_inventory` table.
+//
+//    @function: `findStockedBooks`
+//    @input params: None
+//    @output: [{first_name, last_name, title}]
+//
+//    In this exercise you will ALSO have to write the assertions. For inspiration,
+//    look at the assertions in Exercises 1 - 3.
+//
+//    Expected Result:
+//    [ {first_name: 'Frank',  title: 'Dune', last_name: 'Herbert'},
+//      {title: 'The Cat in the Hat', first_name: 'Theodor Seuss', last_name: 'Geisel'}]
+//
+// */
+let findStockedBooks = db.any('')
+SELECT
+title, first_name, last_name
+FROM book AS title
+INNER JOIN
+authors as auth
+ON
+title.author_id = auth.author_id
+INNER JOIN
+daily_inventory
+ON
 
-
-/* -----------------------------------------
-   Exercise 4
-   -----------------------------------------
-
-   Implement the function `findBookAuthors` which returns the `first_name` and
-   `last_name` from the `authors` table, and the `title` of the
-   books(from the `books` table) that the authors have written.
-
-   @function: `findBookAuthors`
-   @input params: None
-   @output: [{first_name, last_name, title}]
-
-   In this exercise you will ALSO have to write the assertions. For inspiration,
-   look at the assertions in Exercises 1 - 3.
-
-   Expected Result:
-   [{first_name: 'John', last_name: 'Worsley', title: 'Practical PostgreSQL'}
-   {first_name: 'Paulette', last_name: 'Bourgeois', title: 'Franklin in the Dark'}
-   {first_name: 'Margery Williams', last_name: 'Bianco', title: 'The Velveteen Rabbit'}
-   {first_name: 'Louisa May', last_name: 'Alcott', title: 'Little Women'}
-   {first_name: 'Stephen', last_name: 'King', title: 'The Shining'}
-   {first_name: 'Frank', last_name: 'Herbert', title: 'Dune'}
-   {first_name: 'Burne', last_name: 'Hogarth', title: 'Dynamic Anatomy'}
-   {first_name: 'Margaret Wise', last_name: 'Brown', title: 'Goodnight Moon'}
-   {first_name: 'Edgar Allen', last_name: 'Poe', title: 'The Tell-Tale Heart'}
-   {first_name: 'Mark', last_name: 'Lutz', title: 'Learning Python'}
-   {first_name: 'Mark', last_name: 'Lutz', title: 'Programming Python'}
-   {first_name: 'Tom', last_name: 'Christiansen', title: 'Perl Cookbook'}
-   {first_name: 'Arthur C.', last_name: 'Clarke', title: '2001: A Space Odyssey'}
-   {first_name: 'Theodor Seuss', last_name: 'Geisel', title: 'Bartholomew and the Oobleck'}
-   {first_name: 'Theodor Seuss', last_name: 'Geisel', title: 'The Cat in the Hat'}]
-*/
-let findBookAuthors; // IMPLEMENT THIS FUNCTION
-
-/* --------End of Exercise 4---------------- */
-
-
-
-
-
-/* -----------------------------------------
-   Exercise 5
-   -----------------------------------------
-
-   Implement the function `authorIdWithTwoBooks` which returns the
-   `author_id` of authors who have 2 books. (HINT: you have to use a SUBQUERY)
-
-   @function: `authorIdWithTwoBooks`
-   @input params: None
-   @output: [{first_name, last_name, title}]
-
-   In this exercise you will ALSO have to write the assertions. For inspiration,
-   look at the assertions in Exercises 1 - 3.
-
-   Expected Result:
-     [{author_id: 1809},
-      {author_id: 7805}]
-
-
-*/
-let authorIdWithTwoBooks; // IMPLEMENT THIS FUNCTION
-
-/* --------End of Exercise 5---------------- */
-
-
-
-
-
-/* -----------------------------------------
-   Exercise 6
-   -----------------------------------------
-
-   Implement the function `bookTitlesWithMultipleEditions` which returns the
-   `title` of books which have more than 2 editions. (HINT: you have to use a join)
-
-   @function: `bookTitlesWithMultipleEditions`
-   @input params: None
-   @output: [{title}]
-
-   In this exercise you will ALSO have to write the assertions. For inspiration,
-   look at the assertions in Exercises 1 - 3.
-
-   Expected Result:
-     [{title: 'The Shining'},
-      {title: 'The Cat in the Hat'},
-      {title: 'Dune'}
-      {title: '2001: A Space Odyssey'}
-      {title: 'The Tell-Tale Heart'}]
-
-*/
-let bookTitlesWithMultipleEditions; // IMPLEMENT THIS FUNCTION
-
-/* --------End of Exercise 6---------------- */
-
-
-
-
-/* -----------------------------------------
-   Exercise 7
-   -----------------------------------------
-
-   Implement the function `findStockedBooks` which returns the `title` & the
-   author's `first_name` & `last_name` of all books which are stocked as
-   represented in the `daily_inventory` table.
-
-   @function: `findStockedBooks`
-   @input params: None
-   @output: [{first_name, last_name, title}]
-
-   In this exercise you will ALSO have to write the assertions. For inspiration,
-   look at the assertions in Exercises 1 - 3.
-
-   Expected Result:
-   [ {first_name: 'Frank',  title: 'Dune', last_name: 'Herbert'},
-     {title: 'The Cat in the Hat', first_name: 'Theodor Seuss', last_name: 'Geisel'}]
-
-*/
-let findStockedBooks; // IMPLEMENT THIS FUNCTION
-
-/* --------End of Exercise 7---------------- */
-
-
-
+// /* --------End of Exercise 7---------------- */
+//
+//
+//
 
 console.log('Reached the end!');
 pg.end();
